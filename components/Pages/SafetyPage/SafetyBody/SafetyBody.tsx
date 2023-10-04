58a5a251f8a81d9406ec019525f7c8f33224b3a7
Eekstr1m <vinsentrogovskiy21@gmail.com>
2023-10-04 16:45:12 +0300

    safety changes

--- components/Pages/SafetyPage/SafetyBody/SafetyBody.tsx
+++ components/Pages/SafetyPage/SafetyBody/SafetyBody.tsx
@@ -1,11 +1,25 @@
 import Image from "next/image";
 import s from "./SafetyBody.module.scss";
 import photo1 from "@/assets/safety1.jpg";
+import photo2 from "@/assets/safety2.jpg";
 
 export default function SafetyBody() {
   return (
     <section>
       <div className={s.container}>
+        <div className={s.image}>
+          <Image
+            src={photo1}
+            alt="Services body photo"
+            width={500}
+            height={500}
+            style={{
+              objectFit: "cover",
+              borderRadius: "10px",
+              filter: "brightness(85%)",
+            }}
+          />
+        </div>
         <div className={s.content}>
           <div className={s.content_wrapper}>
             <p className={s.title}>Ensuring Workplace Safety</p>
@@ -37,7 +51,7 @@ export default function SafetyBody() {
         </div>
         <div className={s.image}>
           <Image
-            src={photo1}
+            src={photo2}
             alt="Services body photo"
             width={500}
             height={500}
